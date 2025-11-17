5e24f2da1575c9cb9a2c5bbe6b5df9fc285f727b
Mikael Larsson <mikael.j.larsson@hotmail.com>
2025-11-17 22:45:12 +0100

    fix: set dynamic VITE_BASE_PATH for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: '/stiiiks-challenges/',
+  base: process.env.VITE_BASE_PATH || "/",
   plugins: [svelte()],
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        challenges: resolve(__dirname, 'challenges.html'),
-        admin: resolve(__dirname, 'admin.html'),
+        main: resolve(__dirname, "index.html"),
+        challenges: resolve(__dirname, "challenges.html"),
+        admin: resolve(__dirname, "admin.html"),
       },
     },
   },

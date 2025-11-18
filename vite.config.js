2be15504c7c613879f7a50f63d80b135e9d11b5f
Mikael Larsson <mikael.j.larsson@hotmail.com>
2025-11-18 10:26:44 +0100

    fix: enhance VITE_BASE_PATH handling for production and development environments

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
 
+const repoBasePath = "/stiiiks-challenges/";
+
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/",
+  base:
+    process.env.VITE_BASE_PATH ||
+    (process.env.NODE_ENV === "production" ? repoBasePath : "/"),
   plugins: [svelte()],
   build: {
     rollupOptions: {
